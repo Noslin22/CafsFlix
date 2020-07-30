ae463daa632d870a1264fd799fb48f1192430ed5
Joao <joaonilsoneto2@gmail.com>
2020-07-30 20:37:45 -0300

    aula 4 fim

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -31,7 +31,7 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = 'https://db-cafsflix.herokuapp.com/categorias';
     fetch(URL).then(async (response) => {
       const responseJson = await response.json();
       setCategorias([
