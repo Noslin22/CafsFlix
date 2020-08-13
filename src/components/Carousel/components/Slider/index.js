55de4429b89992f6b455ed16380f2acdcb30add0
Joao <joaonilsoneto2@gmail.com>
2020-08-13 15:39:35 -0300

    test

--- src/components/Carousel/components/Slider/index.js
+++ src/components/Carousel/components/Slider/index.js
@@ -39,68 +39,8 @@ const Container = styled.ul`
   .slick-next {
     right: 16px;
   }
-  @media not all and (max-width: 1200px) {
-    .slick-current a:hover,
-    .slick-current a:focus {
-      transform: scale(var(--videoCard-focus-scale)) translateX(12%);
-      z-index: 100;
-      border: 0;
-      opacity: 1;
-    }
-    .slick-active:not(.slick-current) {
-      a:hover, a:focus {
-        transform: scale(var(--videoCard-focus-scale));
-        z-index: 100;
-        border: 0;
-        opacity: 1;
-      }
-    }
-  }
-  @media (max-width: 1200px) {
-    .slick-current {
-      a {
-        transform: scale(var(--videoCard-focus-scale));
-        z-index: 100;
-        border: 0;
-        opacity: 1;
-      }
-    }
-    /* slide when active (when play last to first) */
-    .slick-slide[aria-hidden="true"][tabindex="-1"] + .slick-center {
-      a {
-        transform: scale(var(--videoCard-focus-scale));
-        z-index: 100;
-        border: 0;
-        opacity: 1;
-      }
-    }
-    /* slide when active (when play first to last) */
-    .slick-slide[data-index="-1"] .slick-center {
-      a {
-          transform: scale(var(--videoCard-focus-scale));
-          z-index: 100;
-          border: 0;
-          opacity: 1;
-      }
-    }
-  }
-  @media (max-width: 800px) {
-    --height-mobile: calc(calc(var(--videoCard-container-height-mob) * var(--videoCard-focus-scale-mob)) * 1.02);
-    height: var(--height-mobile);
-    .slick-current, .slick-slide[data-index="-1"] .slick-center, .slick-slide[aria-hidden="true"][tabindex="-1"] + .slick-center {
-      a {
-          transform: scale(var(--videoCard-focus-scale-mob));
-          z-index: 100;
-          border: 0;
-          opacity: 1;
-      }
-    }
-    .slick-prev {
-      left: 5px;
-    }
-    .slick-next {
-      right: 5px;
-    }
+  .slick-current {
+    opacity: 1;
   }
 `;
 
@@ -113,7 +53,6 @@ export const SliderItem = styled.li`
     object-fit: cover;
   }
 `;
-
 const Slider = ({ arrowColor, children }) => (
   <Container arrowColor={arrowColor}>
     <SlickSlider
@@ -124,7 +63,6 @@ const Slider = ({ arrowColor, children }) => (
         slidesToShow: 4,
         slidesToScroll: 2,
         arrows: true,
-        centerMode: false,
         speed: 800,
         dots: false,
 
